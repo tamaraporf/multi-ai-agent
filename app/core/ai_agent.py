701583dddc469039ebeaafccc23585566fc9507d
Tâmara Porfiro <77836325+tamaraporf@users.noreply.github.com>
2026-02-20 14:56:08 -0300

    first commit

---
+++ app/core/ai_agent.py
@@ -0,0 +1,29 @@
+from langchain_groq import ChatGroq
+from langchain_tavily import TavilySearch
+
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages.ai import AIMessage
+
+from app.config.settings import settings
+
+def get_response_from_ai_agents(llm_id, query, allow_search, system_prompt):
+
+    llm = ChatGroq(model=llm_id)
+
+    tools = [TavilySearch(max_results=2)] if allow_search else []
+
+    agent = create_react_agent(
+        model=llm,
+        tools=tools,
+        prompt=system_prompt  # ← MUDANÇA AQUI: state_modifier → prompt
+    )
+
+    state = {"messages": query}
+
+    response = agent.invoke(state)
+
+    messages = response.get("messages")
+
+    ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+
+    return ai_messages[-1]
---
+++ app/main.py
@@ -0,0 +1,53 @@
+import subprocess
+import threading
+import time
+from dotenv import load_dotenv
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger=get_logger(__name__)
+
+load_dotenv()
+
+def run_backend():
+    try:
+        logger.info("starting backend service..")
+        subprocess.run(
+            ["uvicorn", "app.backend.api:app", "--host", "127.0.0.1", "--port", "9999"],
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        logger.exception(
+            "Backend service exited with non-zero status",
+            extra={"returncode": e.returncode, "cmd": e.cmd}
+        )
+        raise CustomException("Failed to start backend", e)
+    except Exception as e:
+        logger.exception("Unexpected error while starting backend")
+        raise CustomException("Failed to start backend", e)
+    
+def run_frontend():
+    try:
+        logger.info("Starting Frontend service")
+        subprocess.run(["streamlit", "run", "app/frontend/ui.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        logger.exception(
+            "Frontend service exited with non-zero status",
+            extra={"returncode": e.returncode, "cmd": e.cmd}
+        )
+        raise CustomException("Failed to start frontend", e)
+    except Exception as e:
+        logger.exception("Unexpected error while starting frontend")
+        raise CustomException("Failed to start frontend", e)
+    
+if __name__=="__main__":
+    try:
+        threading.Thread(target=run_backend).start()
+        time.sleep(2)
+        run_frontend()
+    
+    except CustomException as e:
+        logger.exception(f"CustomException occured : {str(e)}")
+
+
+    
